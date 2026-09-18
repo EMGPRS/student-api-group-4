7b19891d097fa0f65012864a9cef2bb8b5fbbbf1
dmaramba <dumisani.maramba@gmail.com>
2026-09-18 10:22:52 +0200

    changes

--- StudentRegistration.Api/Program.cs
+++ StudentRegistration.Api/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+using StudentRegistration.Api.Data;
 using StudentRegistration.Api.Models;
 using StudentRegistration.Api.Services;
 
@@ -9,10 +11,21 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddSingleton<IStudentService, StudentService>();
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+    ?? throw new InvalidOperationException("Connection string 'StudentRegisterDb' was not found.");
+
+builder.Services.AddDbContext<StudentDBContext>(options =>
+    options.UseSqlServer(connectionString));
+builder.Services.AddScoped<IStudentService, StudentService>();
 
 var app = builder.Build();
 
+
+await using (var scope = app.Services.CreateAsyncScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<StudentDBContext>();
+    await context.Database.MigrateAsync();
+}
 // Configure the HTTP request pipeline.
 
 app.UseSwagger();
